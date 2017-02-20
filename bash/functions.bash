45ceb29881a8d3c4b08cb8c3e628f33f13270f6c
Nelson Crosby <ncrosby@sourcecomb.com>
2017-02-20 15:34:05 +1300

    [scripts, bash] Move `e` to alias `editor`

--- bash/functions.bash
+++ bash/functions.bash
@@ -2,6 +2,7 @@
 # Aliases
 alias ls="ls -h --color=auto"
 alias temon="watch -d -t -n1 sensors"
+alias editor="$EDITOR"
 
 # Toggle powerline fonts for nvim
 function plon { export POWERLINE_FONTS=true; }
